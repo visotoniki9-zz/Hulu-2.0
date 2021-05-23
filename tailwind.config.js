f4b1c0edc153b21044d8f94297263412730efac0
visotoniki9 <d.simon.ssx0@gmail.com>
2021-05-23 02:38:48 +0200

    Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+/* eslint-disable global-require */
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
--- utils/requests.js
+++ utils/requests.js
@@ -11,38 +11,38 @@ export default {
   },
   fetchActionMovies: {
     title: 'Action',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=28`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: 'Comedy',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=35`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: 'Horror',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=27`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: 'Romance',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=10749`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMysteryMovies: {
     title: 'Mystery',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=9648`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=9648`,
   },
   fetchScifiMovies: {
     title: 'Scifi',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=878`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=878`,
   },
   fetchWesternMovies: {
     title: 'Western',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=37`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=37`,
   },
   fetchAnimationMovies: {
     title: 'Animation',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=16`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=16`,
   },
   fetchTv: {
     title: 'Tv Movie',
-    url: `/discover/movie/?api_key=${API_KEY}&with_generes=10770`,
+    url: `/discover/movie/?api_key=${API_KEY}&with_genres=10770`,
   },
 };
